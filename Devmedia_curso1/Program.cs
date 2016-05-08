8355ecb5ce470f74a89dd4704eded95ef7d280ab
Gilcemar <gilcemargz@gmail.com>
2016-05-08 11:27:49 -0300

    aula types

--- Devmedia_curso1/Program.cs
+++ Devmedia_curso1/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Devmedia_curso1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
@@ -23,9 +25,50 @@ namespace Devmedia_curso1
             Console.WriteLine(f1);
 
 
+            int result = 0;
+            if (int.TryParse("001x", out result))
+            {
+                Console.WriteLine("Converteu");
+            }
+            else
+            {
+                Console.WriteLine("Não Converteu");
+            };
 
-            Console.Write("olá mundo");
+            object o = i1;
+
+            Console.WriteLine(i1.ToString());
+
+            Console.WriteLine("olá mundo");
+
+
+            int[] arra1 = new int[5];
+
+
+            arra1[0] = 10;
+            arra1[1] = 12;
+
+            Console.WriteLine(arra1[0]);
+
+
+            ArrayList arl = new ArrayList();
+
+            arl.Add(10);
+            arl.Add(2);
+
+
+            Console.WriteLine(arl[1]);     
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
+
         }
     }
 }
